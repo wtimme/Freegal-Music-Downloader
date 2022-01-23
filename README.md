f7357f11812276a9924aed88d90a22c2da6d8fb9
wtimme <wtimme@users.noreply.github.com>
2022-01-23 22:53:51 +0100

    Add login

--- README.md
+++ README.md
@@ -4,4 +4,23 @@ Script for automatically downloading your music from Freegal.
 
 ## Running the script
 
-    % USERNAME="john.doe" PASSWORD="top-secret" LIBRARY_ID=1234 node index.js
+First, install the dependencies:
+
+    % npm install
+
+To pass the credentials to the script, you can either prepend them to the script invocation, like so:
+
+    % USERNAME="john.doe" PASSWORD="top-secret" LIBRARY_ID=1234 npm start
+
+Or, you can add them to an `.env` file, like so:
+
+```
+# .env
+USERNAME="john.doe"
+PASSWORD="top-secret"
+LIBRARY_ID=1234
+```
+
+Make sure not to commit this file, though. You can now run the script using
+
+    % npm start
--- index.js
+++ index.js
@@ -1,5 +1,35 @@
+require('dotenv').config();
+
+// Configuration
+let baseURL = 'https://api.freegalmusic.com/v1';
+let bearerToken = 'N2ZmNTNlZDRiN2FiNWRjN2EwOWY3MDkwYWVlZmE5YzkyZmRlNTA2OGRjYzIwMTlkMGUyMTFlZDE4YTg5ZDI3Ng';
+
 // Pass credentials via environment variables
 let username = process.env.USERNAME;
 let password = process.env.PASSWORD;
 let libraryId = process.env.LIBRARY_ID;
 
+const axios = require('axios').create({
+  baseURL: baseURL,
+  headers: { 'Authorization': 'Bearer ' + bearerToken }
+});
+
+const login = (username, password, libraryId) => {
+  return new Promise((resolve, reject) => {
+    axios.post(`/login`, {
+      identifier1: username,
+      identifier2: password,
+      libraryId: libraryId
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  });
+};
+
+login(username, password, libraryId)
+  .then(data => console.log(data))
+  .catch(err => console.error(err));
